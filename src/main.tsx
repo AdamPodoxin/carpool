9f6534a4f22d91f7684c98f28981fa2434c39c2f
Adam Podoxin <31609249+AdamPodoxin@users.noreply.github.com>
2024-01-20 15:54:47 -0800

    Set up create page

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import App from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { AUTH_CLIENT_ID, AUTH_DOMAIN } from "./lib/constants.ts";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import CreatePage from "./pages/Create/index.tsx";
 import JoinRidePage from "./pages/JoinRide/index.tsx";
+import CreatePage from "./pages/Create/index.tsx";
 
 const router = createBrowserRouter([
 	{
@@ -13,12 +13,12 @@ const router = createBrowserRouter([
 		element: <App />,
 	},
 	{
-		path: "/create",
-		element: <CreatePage />,
+		path: "/joinRide/:rideId",
+		element: <JoinRidePage />
 	},
 	{
-		path: "/joinRide/:rideId",
-		element: <JoinRidePage />,
+		path: "/create",
+		element: <CreatePage />,
 	},
 ]);
 
