f2083fe45bc8802ba1968168cf64ec56bc237b22
Adam Podoxin <31609249+AdamPodoxin@users.noreply.github.com>
2024-01-20 15:56:35 -0800

    Add create ride button to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 import LogoutButton from "../../components/LogoutButton";
 
 const HomePage = () => {
 	return (
 		<>
 			<LogoutButton />
+
+			<br />
+
+			<Link to={"/create"}>Create a Ride</Link>
 		</>
 	);
 };
