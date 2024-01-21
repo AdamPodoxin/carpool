20cb4bf8e584dfe34831fc184aab061fab025ba3
Adam Podoxin <31609249+AdamPodoxin@users.noreply.github.com>
2024-01-20 20:52:58 -0800

    Add basic rides list on home page

--- src/lib/rideService.ts
+++ src/lib/rideService.ts
@@ -1,6 +1,6 @@
 import { db } from "./firebase";
 import { Ride } from "./types";
-import { addDoc, collection } from "firebase/firestore";
+import { Timestamp, addDoc, collection, getDocs } from "firebase/firestore";
 
 export async function createRide(
 	driverSub: string,
@@ -59,3 +59,19 @@ async function putRide(
 
 	return rideRef.id;
 }
+
+export const getAllRides = async () => {
+	const querySnapshot = await getDocs(collection(db, "rides"));
+	const rides = querySnapshot.docs.map((doc) => {
+		const data = doc.data();
+		const ride = data as Ride;
+
+		ride.id = doc.id;
+		ride.startTime = (data.startTime as Timestamp).toDate();
+
+		ride.startTime;
+
+		return ride;
+	});
+	return rides;
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,56 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
+import { useEffect, useState } from "react";
+import { Ride } from "../../lib/types";
+import { useAuth0 } from "@auth0/auth0-react";
+import { getAllRides } from "../../lib/rideService";
+
+import "./style.css";
+
+const RideInfo = ({ ride }: { ride: Ride }) => {
+	return (
+		<>
+			<div className="rideInfo">
+				<p>From: {ride.origin}</p>
+				<p>To: {ride.destination}</p>
+				<p>At: {ride.startTime.toLocaleString()}</p>
+				<p>Seats left: {ride.capacity}</p>
+
+				<Button onClick={() => {}}>Join</Button>
+			</div>
+		</>
+	);
+};
 
 const HomePage = () => {
+	const [rides, setRides] = useState<Ride[]>([]);
+
+	const { user } = useAuth0();
+
 	const navigate = useNavigate();
+
+	const fetchRides = async () => {
+		const rides = await getAllRides();
+		setRides(rides);
+	};
+
+	useEffect(() => {
+		fetchRides();
+	}, []);
 
 	return (
 		<>
 			<Button onClick={() => navigate("/create")}>Create a ride</Button>
+
+			{!!rides.length && (
+				<div className="ridesList">
+					{rides
+						.filter((ride) => ride.capacity > 0)
+						.map((ride) => (
+							<RideInfo key={ride.id} ride={ride} />
+						))}
+				</div>
+			)}
 		</>
 	);
 };
