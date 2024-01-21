7509fc918bad9030b9d7b9c53a3cfb442f5f32f0
aria231 <aria231@students.cs.ubc.ca>
2024-01-20 21:30:47 -0800

    format join ride infos

--- src/pages/JoinRide/RideInfo.tsx
+++ src/pages/JoinRide/RideInfo.tsx
@@ -1,18 +1,26 @@
 import { Ride } from "../../lib/types";
+// import firebase from "../../lib/firebase"
 
 type RideInfoProps = {
     ride: Ride;
 }
 
 const RideInfo = ({ride}: RideInfoProps) => {
+    const pickupTimestamp = ride.startTime as any;
+    const timestamp = {
+        seconds: pickupTimestamp.seconds,
+        nanoseconds: pickupTimestamp.nanoseconds
+    }
+    const firebaseDate = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
+    const pickupTime = firebaseDate.toDateString() + " " + firebaseDate.toTimeString();
 
     return (
         <>
             <h2>Ride info</h2>
             <p>
                 Origin: {ride.origin}<br/>
-                Destination: {ride.destination}
-                {/* Pickup Time: {ride.startTime.toISOString()} */}
+                Destination: {ride.destination}<br/>
+                Pickup Time: {pickupTime}<br/>
             </p>
         </>
     )
--- src/pages/JoinRide/index.tsx
+++ src/pages/JoinRide/index.tsx
@@ -14,27 +14,18 @@ const JoinRidePage = () => {
 
     const getRideInfo = async () => {
         const rideDoc = await getDoc(doc(db, "rides", rideId!))
-        console.log(rideDoc.data());
         setRide(rideDoc.data() as Ride)
     };
 
     const getVehicleInfo = async () => {
         const vehicleDoc = await getDoc(doc(db, "vehicles", ride!.vehicleId));
-        console.log(vehicleDoc.data());
         setVehicle(vehicleDoc.data() as Vehicle);
     }
 
-    const getInfos = async () => {
-        await getRideInfo().then(() => {getVehicleInfo();})
-    }
-
     useEffect(() => {
-        // getRideInfo().then(() => {getVehicleInfo()});
         getRideInfo();
-        console.log("Ride: " + ride);
-        getVehicleInfo();
-        // getInfos();
-    }, [])
+        {ride && getVehicleInfo()};
+    }, [ride])
 
     return (
         <>
