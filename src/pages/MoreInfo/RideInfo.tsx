6a5ab15a2f4faceb9b7553f93356831ebadcd2be
Adam Podoxin <31609249+AdamPodoxin@users.noreply.github.com>
2024-01-21 10:11:55 -0800

    Rider names list

--- src/pages/MoreInfo/RideInfo.tsx
+++ src/pages/MoreInfo/RideInfo.tsx
@@ -1,29 +1,49 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Ride } from "../../lib/types";
-// import firebase from "../../lib/firebase"
+
+const RiderNamesList = ({ riderNames }: { riderNames: string[] }) => (
+	<>
+		<h2>Riders</h2>
+		{riderNames.map((riderName) => (
+			<p key={riderName}>{riderName}</p>
+		))}
+	</>
+);
 
 type RideInfoProps = {
-    ride: Ride;
-}
+	ride: Ride;
+};
 
-const RideInfo = ({ride}: RideInfoProps) => {
-    const pickupTimestamp = ride.startTime as any;
-    const timestamp = {
-        seconds: pickupTimestamp.seconds,
-        nanoseconds: pickupTimestamp.nanoseconds
-    }
-    const firebaseDate = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
-    const pickupTime = firebaseDate.toDateString() + " " + firebaseDate.toTimeString();
+const RideInfo = ({ ride }: RideInfoProps) => {
+	const pickupTimestamp = ride.startTime as any;
+	const timestamp = {
+		seconds: pickupTimestamp.seconds,
+		nanoseconds: pickupTimestamp.nanoseconds,
+	};
+	const firebaseDate = new Date(
+		timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+	);
+	const pickupTime =
+		firebaseDate.toDateString() + " " + firebaseDate.toTimeString();
 
-    return (
-        <>
-            <h2>Ride info</h2>
-            <p>
-                Origin: {ride.origin}<br/>
-                Destination: {ride.destination}<br/>
-                Pickup Time: {pickupTime}<br/>
-            </p>
-        </>
-    )
-}
+	const { user } = useAuth0();
+
+	return (
+		<>
+			<h2>Ride info</h2>
+			<p>
+				Origin: {ride.origin}
+				<br />
+				Destination: {ride.destination}
+				<br />
+				Pickup Time: {pickupTime}
+				<br />
+			</p>
+			{ride.driverSub === user?.sub && (
+				<RiderNamesList riderNames={ride.riderNames} />
+			)}
+		</>
+	);
+};
 
 export default RideInfo;
